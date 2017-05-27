0b1b6223fdd0e181af2dd0108e078dfc9c416285
perezyuan <yzmspirit@sina.cn>
2017-05-27 16:17:53 +0800

    modify

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import fullName from './fullName';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import fullName from './fullName';
 
-class HelloReact extends React.Component {
-    render() {
-        return(
-            <div>{ fullName }</div>
-        );
-    }
-}
+// class HelloReact extends React.Component {
+//     render() {
+//         return(
+//             <div>{ fullName }</div>
+//         );
+//     }
+// }
 
-ReactDOM.render(<HelloReact />, document.getElementById("app"));
+// ReactDOM.render(<HelloReact />, document.getElementById("app"));
 
 // if (module.hot) {
 //     module.hot.accept();
 // }
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FullName from './FullName';
+import App from './App';
+
+ReactDOM.render(<FullName />, document.getElementById('app'));
--- src/fullName.jsx
+++ src/fullName.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
 
-const user = {
-  firstName: 'Perez',
-  lastName: 'Yuan'
-};
+// const user = {
+//   firstName: 'Perez',
+//   lastName: 'Yuan'
+// };
 
-const element = (
+/*const element = (
   <h1>
     Hello, { formatName(user) }!
   </h1>
-);
+);*/
 
-export default element;
+// 必须大写
+class FullName extends React.Component {
+  render() {
+    return (
+      <div>
+        bbb
+      </div>
+    );
+  }
+}
+
+export default FullName;
